5fbdadd83e6d4e41b97dacf49e26ce413072122f
Brandon <bmushori@gmail.com>
2021-07-24 18:27:00 +0200

    Form Control

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React from "react";
 import './App.css';
+import { 
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core"
 
 function App() {
   return (
     <div className="app">
       <h1>Covid 19 tracker app</h1>
+      <FormControl className="app__dropdown"></FormControl>
+      <Select 
+        variant="outlined"
+        value="abc"
+      >
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+      </Select>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
