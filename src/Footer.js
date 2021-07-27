ca58920bae1504ac884c3e8090a7055b4239913c
Brandon <bmushori@gmail.com>
2021-07-27 18:17:05 +0200

    Style changes

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            Made with awesomeness by Brandon
+        </footer>
+    )
+}
+
+export default Footer
